76be445560edc294f262767233dd45abb6540907
chongyuelinn-nus <32475137+chongyuelinn-nus@users.noreply.github.com>
2017-11-27 15:42:37 +0800

    tidied code

--- src/upload.h
+++ src/upload.h
@@ -1,10 +1,6 @@
 #ifndef UPLOAD_GPU
 #define UPLOAD_GPU
 
-//void cudaCopy(void ** dst, void ** src, int bytes);
 void cudaCopy(unsigned char* dst, unsigned char * src, int bytes);
-void cuda2HostArray(cudaArray* c_array, unsigned char* h_array, int width, int height, int bytes);
-void cudaMemCpyArr(cudaArray* c_array, unsigned char* h_array, int width, int height, int bytes);
-//void cudaMemCpyArr(const cudaArray_t * c_array, unsigned char* h_array, int width, int height, int bytes)
-// void cudaMemCpyArr(const struct cudaArray * c_array, unsigned char* h_array, int width, int height, int bytes);
+
 #endif
